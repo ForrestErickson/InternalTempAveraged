7043ccf83a7c42c9ae6474d5c1b1747fabefbd61
Forrest Erickson <forresterickson@comcast.net>
2022-01-03 15:50:58 -0500

    Initial commit. Not much more than program name and some references.

---
+++ InternalTempAveraged.ino
@@ -0,0 +1,28 @@
+/*InternalTempAveraged
+ * 
+ * Author: Forrest Lee Erickson
+ * Date: 20220103
+ * License: Public domain, Free for all use
+ * Warrantee: This program is designed to kill you 
+ * and render the earth uninhabitable but is not guarinteed to do so
+ * 
+ * Brief: Measure interntal ADC8 for temprature.
+ * See ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061B.pdf section 24.8 (page 256)
+ * 
+ * Exponentially weighted moving average see:
+ * https://en.wikipedia.org/wiki/Moving_average#Exponentially_weighted_moving_variance_and_standard_deviation
+ * 
+ */
+
+
+ 
+ 
+ void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
